4b87c6e912a9c35f34038ce9182f9c036924534e
nellievropa <nellievropa23@gmail.com>
2023-06-20 17:25:12 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль 
+// текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+const navEl = document.querySelector('.categories');
+// categories -це ID - звернути увагу!
+console.log('Number of categories:', navEl.children.length);
+
+// вибираємо всіх дітей батька 
+console.log(navEl.querySelectorAll('.item'));
