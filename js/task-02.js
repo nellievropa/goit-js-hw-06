6ae219bb60f572d10ead776a606331069bb5ff9f
nellievropa <nellievropa23@gmail.com>
2023-06-22 09:51:56 +0300

    ДЗ 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,72 +7,57 @@ const ingredients = [
   'Condiments',
 ];
 
-const navItemEl = document.createElement('li');
-navItemEl.textContent = 'Potatoes';
-navItemEl.classList.add('item');
+// const navItemEl = document.createElement('li');
+// navItemEl.textContent = 'Potatoes';
+// navItemEl.classList.add('item');
 
-console.log(navItemEl);
-
-const navItemEl1 = document.createElement('li');
-navItemEl.textContent = 'Mushrooms';
-navItemEl.classList.add('item');
-
-console.log(navItemEl1);
-
-const navItemEl2 = document.createElement('li');
-navItemEl.textContent = 'Garlic';
-navItemEl.classList.add('item');
-console.log(navItemEl2);
-
-
-const navItemEl3 = document.createElement('li');
-navItemEl.textContent = 'Tomatos';
-navItemEl.classList.add('item');
-console.log(navItemEl3);
-
-
-const navItemEl4 = document.createElement('li');
-navItemEl.textContent = 'Herbs';
-navItemEl.classList.add('item');
-console.log(navItemEl4);
-
-const navItemEl5 = document.createElement('li');
-navItemEl.textContent = 'Condiments';
-navItemEl.classList.add('item');
-console.log(navItemEl5);
-
-const list = document.querySelector('ingredients');
-
-
-// list.prepend(navItemEl, navItemEl1, navItemEl2, navItemEl3, navItemEl4, navItemEl5);
-
-// console.log(list);
+// console.log(navItemEl);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-const ingredientsContainerEl1 = document.querySelector('.ingredients');
+const list = document.querySelector('#ingredients');
 const elements = ingredients.map(option => {
-  const navItemEl = document.createElement('li');
-  navItemEl.textContent = 'Potatoes';
-  navItemEl.classList.add('item');
-
-    return navItemEl;
+      const itemEl = document.createElement('li');
+      itemEl.textContent = option;
+      itemEl.classList.add('item');
     
-});
+        return itemEl;
+        
+    });
+    
 
-console.log(navItemEl);
+
 console.log(elements);
-// ingredientsContainerEl.append(elements);
+list.append(...elements);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const option = ingredients;
+// const ingredientsContainerEl1 = document.querySelector('.ingredients');
+// const elements = ingredients.map(option => {
+//   const navItemEl = document.createElement('li');
+//   navItemEl.textContent = 'Potatoes';
+//   navItemEl.classList.add('item');
+
+//     return navItemEl;
+    
+// });
+
+// console.log(navItemEl);
+// console.log(elements);
+// ingredientsContainerEl.append(navItemEl);
